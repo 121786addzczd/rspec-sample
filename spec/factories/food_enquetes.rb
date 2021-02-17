a76d83c8de69b4f88b5058b00f82b0d159ffc002
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-02-17 20:53:50 +0900

    FactoryBotで効率的にテストデータを用意

--- spec/factories/food_enquetes.rb
+++ spec/factories/food_enquetes.rb
@@ -1,5 +1,36 @@
 FactoryBot.define do
   factory :food_enquete do
-    
   end
+
+  factory :food_enquete_tanaka, class: 'FoodEnquete' do
+    name { '田中 太郎' }
+    mail { 'taro.tanaka@example.com' }
+    age { 25 }
+    food_id { 2 }
+    score { 3 }
+    request { 'おいしかったです。' }
+    present_id { 1 }
+  end
+
+  # 田中 太郎」と「山田 次郎」を区別する必要があるので、food_enquete_yamadaというFactory名、FoodEnqueteというクラス名で宣言
+  factory :food_enquete_yamada, class: 'FoodEnquete' do
+    name { '山田 次郎' }
+    mail { 'jiro.yamada@example.com' }
+    age { 22 }
+    food_id { 1 }
+    score { 2 }
+    request { '' }
+    present_id { 0 }
+  end
+
+  factory :food_enquete_sato, class: 'FoodEnquete' do
+    name { '佐藤 仁美' }
+    mail { 'hitomi.sato@example.com' }
+    age { 19 }
+    food_id { 2 }
+    score { 3 }
+    request { 'おいしかったです。' }
+    present_id { 1 }
+  end
+
 end
